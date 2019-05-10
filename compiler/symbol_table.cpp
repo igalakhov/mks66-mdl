93719cbb8b9c7bc592ad48470e7edfa8195d8b1f
Ivan Galakhov <igalakhov00@stuy.edu>
2019-05-10 15:33:17 -0400

    this is gonna take a while

---
+++ compiler/symbol_table.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ivan Galakhov on 2019-05-10.
+//
+
+#include "symbol_table.h"
---
+++ compiler/symbol_table.h
@@ -0,0 +1,14 @@
+//
+// Created by Ivan Galakhov on 2019-05-10.
+//
+
+#ifndef WORK_01_LINE_SYMBOL_TABLE_H
+#define WORK_01_LINE_SYMBOL_TABLE_H
+
+
+class symbol_table {
+
+};
+
+
+#endif //WORK_01_LINE_SYMBOL_TABLE_H
